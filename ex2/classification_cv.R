91db1a45eb1f63796141824d1eccea99cca55123
e.alcobaca <e.alcobaca@gmail.com>
2018-09-12 10:43:51 -0300

    ex2 updated

--- ex2/classification_cv.R
+++ ex2/classification_cv.R
@@ -1,12 +1,6 @@
 library(OpenML)
 library(mlr)
 
-# List OpenML data sets.
-cat("OpenML datasets: ...\n")
-list_datasets = listOMLDataSets()
-print(head(list_datasets))
-cat("\n\n")
-
 # Get a dataset
 # more information https://www.openml.org/search?q=cancer&type=data
 data_obj = getOMLDataSet(data.id=61)
@@ -14,21 +8,33 @@ dataset = data_obj$data
 
 # classification
 task = makeClassifTask(data = dataset, target = "class")
-cat("Classification Task: ...\n")
-print(task)
-cat("\n\n")
 
 # Classification algorithm
 classif.rf = makeLearner("classif.randomForest")
 
-### Holdout estimation
-rdesc = makeResampleDesc("CV", iters=5)
+# measures
+me = list(acc, bac)
 
-### Calculate the performance
+# 10-cv 
+rdesc = makeResampleDesc("CV", iters=10, stratify=TRUE)
+
+# Calculate the performance
 r = resample(learner=classif.rf, task=task, resampling=rdesc,
-             measures=list(acc, bac), show.info = TRUE)
+             measures=me, show.info = TRUE)
+cat("--------------------------------------------------------------\n\n")
 
 pred = getRRPredictions(r)
 print(pred)
+cat("--------------------------------------------------------------\n\n")
 
+# 10-cv with 30 rep 
+rdesc.rep = makeResampleDesc("RepCV", reps=30, stratify=TRUE)
 
+# Calculate the performance
+r = resample(learner=classif.rf, task=task, resampling=rdesc.rep,
+             measures=me, show.info = TRUE)
+cat("--------------------------------------------------------------\n\n")
+
+pred.rep = getRRPredictions(r)
+print(pred.rep)
+cat("--------------------------------------------------------------\n\n")
